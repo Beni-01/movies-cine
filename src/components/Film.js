f9ddc993c90c0309dbfb3fff165934cb7340b477
Béni MAMPUNINA <contactmampbeni@gmail.com>
2021-05-07 12:19:32 +0100

    refactor: add content to page Film

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Footer } from "./footer";
+import { Categories } from "./Categories";
+import { Layout } from "./CardList";
 
 export const Film = (props) => {
   return (
     <>
-      <h1 className="text-muted">Film PAGE</h1>
+      <Categories />
+      <Layout cardclassName="img-fluid p-1 img-style-film" />
+      <Layout cardclassName="img-fluid p-1 img-style-film" />
+      <Layout cardclassName="img-fluid p-1 img-style-film" />
+      <Layout cardclassName="img-fluid p-1 img-style-film" />
+      <Layout cardclassName="img-fluid p-1 img-style-film" />
+
+      <Footer />
     </>
   );
 };
